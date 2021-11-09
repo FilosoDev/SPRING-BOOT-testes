54624b23afe38457a55578a641429fe5fe3650e5
Arthur Ferreira <arthurferreira@usp.br>
2021-11-09 14:53:08 -0300

    tutorial 01

---
+++ rh/src/main/java/br/rhsolutions/contatos/modelo/ContatoRepositorio.java
@@ -0,0 +1,11 @@
+package br.rhsolutions.contatos.modelo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "contatos", path = "contatos")
+public interface ContatoRepositorio extends JpaRepository<Contato, Long> {
+
+	
+	
+}
