b5277b4f7ff23d0fa57213f4fb21b3d0191a32f5
Arthur <arthurferreira@usp.br>
2021-11-20 17:46:32 -0300

    arrumando

--- rhapp/src/main/java/com/rhapp/rhapp/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rhapp.rhapp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rhapp.rhapp.models.Evento;
-
-public interface EventoRepository extends CrudRepository<Evento, String> {
-	Evento findByCodigo(long codigo);
-}
